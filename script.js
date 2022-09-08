de793607dc5c78c1347629cec95561b1b9cfbb26
Your Name <you@example>
2022-09-08 13:29:59 +0400

    logic is ready

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
 let password = document.querySelector('#password');
 let img = document.querySelector('img');
 
-// let characterValidation = false;
-let num = 600;
+let num = 400;
+let characterChecked = true;
 let numChecked = true;
 let letterChecked = true;
 let lengthChecked = true;
 
 const letter = /[A-Z]/g;
 const numbers = /[0-9]/g;
-const specialCharacter = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+const specialCharacter = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 
 password.addEventListener('keypress', (e) => {
-    // const specialCharacter = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})";
-    // const specialCharacter = /?=.*[!@#$%^&*]/;
 
     if(e.key.match(numbers) && numChecked){
         numChecked = false;
-        num = num - 200;
-        numValidation = true;
+        num = num - 100;
         img.style.filter = `blur(${num}px)`;
         return;
 
     } 
     if(e.key.match(letter) && letterChecked){
         letterChecked = false;
-        num = num - 200;
-        letterValidation = true;
+        num = num - 100;
         img.style.filter = `blur(${num}px)`
-        return
+        return;
     } 
-    // if(e.key.match(specialCharacter)) characterValidation = true;
+    if(e.key.match(specialCharacter) && characterChecked) {
+        characterChecked = false;
+        num = num - 100;
+        img.style.filter = `blur(${num}px)`
+        return;
+    }
+
     if(password.value.length >= 8 && lengthChecked){
         lengthChecked = false;
-        num = num - 200;
-        lengthValidation = true;
+        num = num - 100;
         img.style.filter = `blur(${num}px)`
-        return
+        return;
     } 
-
-    // console.log(characterValidation);
-
-    // if()
-
-
+    
 })
